be9c8a31aae90ab52c588d9975134769b56f0910
Neogamezzz <lyosha555@outlook.com>
2023-05-29 15:12:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rnd = new Random().Next(0, 101);
+            int attempt = 0;
+            Console.WriteLine("I have a number between 0 and 100. Try to guess it.");
+
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                {
+                    Console.WriteLine("Invalid value. Try again.");
+                    continue;
+                }
+                if (userNumber < rnd)
+                {
+                    attempt++;
+                    Console.WriteLine("I’m guessing a bigger number. Try again.");
+                }
+                else if (userNumber > rnd)
+                {
+                    attempt++;
+                    Console.WriteLine("I’m guessing a smaller number. Try again.");
+                }
+                else
+                {
+                    attempt++;
+                    Console.WriteLine($"Congratulations! You guessed my number in {attempt} attempts.");
+                    return;
+                }
+            }
+        }
+    }
+}
